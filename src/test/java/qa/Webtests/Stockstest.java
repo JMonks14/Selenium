f69888a3c7be790ec37134850e185063aff7c2a4
James Monks <James.Monks@academytrainee.com>
2020-08-04 16:10:04 +0100

    stockstest 1st bit

--- src/test/java/qa/Webtests/Stockstest.java
+++ src/test/java/qa/Webtests/Stockstest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -25,12 +26,12 @@ public class Stockstest {
 	    }
 	 
 	 @Test
-	 public void findBiggestRiser() {
-		 driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100");
-		 WebElement rise = driver.findElement(By.xpath("//*[@id=\"view-constituents\"]/ul/li[2]/a/strong"));
-		 rise.click();
+	 public void findBiggestRiser() throws InterruptedException {
+		 driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100/risers");
+//		 WebElement rise = driver.findElement(By.cssSelector("title = 'View risers"));
+//		 rise.click();
 		 
-		 List<WebElement> names = driver.findElements(By.className("name-col-align-left"));
+		 List<WebElement> names = driver.findElements(By.cssSelector("[class^='link-headline {Sedol']"));
 		 List<WebElement> prices = driver.findElements(By.cssSelector("[data-field='change']"));
 		 ArrayList<Double> risers = new ArrayList<Double>();
 		 ArrayList<String> snames = new ArrayList<String>();
@@ -42,9 +43,26 @@ public class Stockstest {
 			 snames.add(name.getText());
 		 }
 		 double maxriser = Collections.max(risers);
-		 int index = risers.indexOf(maxriser);
+		 int index = risers.indexOf(maxriser)-1;
+		 System.out.println(maxriser);
+		 String bRiser=snames.get(index);
+		 System.out.println(bRiser);
 		 
+//		 for (double item:risers) {
+//			 System.out.println(item);
+//		 }
+//		 
+//		 for (String item:snames) {
+//			 System.out.println(item);
+//		 }
+		 
+		 Thread.sleep(10000);
 		 
 	 }
+	 
+	    @After
+	    public void tearDown() {
+	        driver.close();
+	    }
 
 }
